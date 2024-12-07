6645806505de68c6fc327aecfca5714f3e99f0a8
unknown <DanishJamalArain>
2024-12-07 13:12:25 -0800

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Dr Sajjad Chatbot Project',
+    version= '0.0.0',
+    author= 'Muhammad Asim',
+    author_email= 'bsai23108110@szabist.pk',
+    packages= find_packages(),
+    install_requires = []
+
+)
